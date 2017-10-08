6e1c8e9128ab4f469320d8c576535cae64b44c4d
Matthew Williams <matthew.williams@impactradius.com>
2017-10-08 21:57:41 +0200

    Added travis yml for integration with heroku and some tests

--- src/main/java/solo/Application.java
+++ src/main/java/solo/Application.java
@@ -1,18 +1,7 @@
 package solo;
 
-import org.joda.time.DateTime;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import solo.domain.Album;
-import solo.domain.Artist;
-import solo.domain.Song;
-import solo.repository.AlbumRepository;
-import solo.repository.ArtistRepository;
-import solo.repository.SongRepository;
-
-import java.util.Arrays;
 
 /**
  * @author Matthew Williams
@@ -24,16 +13,16 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    @Bean
-    CommandLineRunner init(AlbumRepository albumRepository, ArtistRepository artistRepository, SongRepository songRepository){
-
-        Artist artist = artistRepository.save(new Artist("Buddy"));
-        Album album = albumRepository.save(new Album(artist, "Magnolia", new DateTime(2017, 8, 25, 0, 0).toDate(), 5, 5));
-        songRepository.save(new Song(album, 1, "Buddy", "4 The Record", "Boogie", "Mike & Keys", true));
-        songRepository.save(new Song(album, 2, "Buddy", "Who Shot 2 Tall", null, "Mike & Keys", true));
-        songRepository.save(new Song(album, 3, "Buddy", "Type Of Shit", "Wiz Khalifa", "Mike & Keys", true));
-        songRepository.save(new Song(album, 4, "Buddy", "Last Time", "Kent Jamz", "Mike & Keys", true));
-        songRepository.save(new Song(album, 5, "Buddy", "That Much", null, "Mike & Keys", true));
-        return (evt) -> Arrays.asList(album);
-    }
+//    @Bean
+//    CommandLineRunner init(AlbumRepository albumRepository, ArtistRepository artistRepository, SongRepository songRepository){
+//
+//        Artist artist = artistRepository.save(new Artist("Buddy"));
+//        Album album = albumRepository.save(new Album(artist, "Magnolia", new DateTime(2017, 8, 25, 0, 0).toDate(), 5, 5));
+//        songRepository.save(new Song(album, 1, "Buddy", "4 The Record", "Boogie", "Mike & Keys", true));
+//        songRepository.save(new Song(album, 2, "Buddy", "Who Shot 2 Tall", null, "Mike & Keys", true));
+//        songRepository.save(new Song(album, 3, "Buddy", "Type Of Shit", "Wiz Khalifa", "Mike & Keys", true));
+//        songRepository.save(new Song(album, 4, "Buddy", "Last Time", "Kent Jamz", "Mike & Keys", true));
+//        songRepository.save(new Song(album, 5, "Buddy", "That Much", null, "Mike & Keys", true));
+//        return (evt) -> Arrays.asList(album);
+//    }
 }
