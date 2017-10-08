87379c140cdd381d11b88927f0dc95aea28ed3c0
Matthew Williams <matthew.williams@impactradius.com>
2017-10-08 22:58:41 +0200

    database properties updated

--- src/main/java/solo/Application.java
+++ src/main/java/solo/Application.java
@@ -2,6 +2,13 @@ package solo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
 
 /**
  * @author Matthew Williams
@@ -11,6 +18,16 @@ public class Application {
 
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
+    }
+
+    @Configuration
+    public class DatabaseConfig {
+        @Bean
+        @Primary
+        @ConfigurationProperties(prefix = "spring.datasource")
+        public DataSource dataSource() {
+            return DataSourceBuilder.create().build();
+        }
     }
 
 //    @Bean
