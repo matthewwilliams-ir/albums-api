c53ce13d4c9c37b80f1acbb639d5f710c162a76c
Matthew Williams <matthew.williams@impactradius.com>
2017-12-05 21:33:40 +0200

    Connect to postgres

--- src/main/java/solo/Application.java
+++ src/main/java/solo/Application.java
@@ -29,17 +29,4 @@ public class Application {
             return DataSourceBuilder.create().build();
         }
     }
-
-//    @Bean
-//    CommandLineRunner init(AlbumRepository albumRepository, ArtistRepository artistRepository, SongRepository songRepository){
-//
-//        Artist artist = artistRepository.save(new Artist("Buddy"));
-//        Album album = albumRepository.save(new Album(artist, "Magnolia", new DateTime(2017, 8, 25, 0, 0).toDate(), 5, 5));
-//        songRepository.save(new Song(album, 1, "Buddy", "4 The Record", "Boogie", "Mike & Keys", true));
-//        songRepository.save(new Song(album, 2, "Buddy", "Who Shot 2 Tall", null, "Mike & Keys", true));
-//        songRepository.save(new Song(album, 3, "Buddy", "Type Of Shit", "Wiz Khalifa", "Mike & Keys", true));
-//        songRepository.save(new Song(album, 4, "Buddy", "Last Time", "Kent Jamz", "Mike & Keys", true));
-//        songRepository.save(new Song(album, 5, "Buddy", "That Much", null, "Mike & Keys", true));
-//        return (evt) -> Arrays.asList(album);
-//    }
 }
