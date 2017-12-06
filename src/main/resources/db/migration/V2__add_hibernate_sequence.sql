5b301d977fe2c88d1bec3fcd3aead4b753158522
Matthew Williams <matthew.williams@impactradius.com>
2017-12-06 21:51:28 +0200

    add v2 db migration script

---
+++ src/main/resources/db/migration/V2__add_hibernate_sequence.sql
@@ -0,0 +1,46 @@
+create sequence hibernate_sequence
+;
+
+create table album
+(
+	id bigint not null
+		constraint album_pkey
+		primary key,
+	confirmed integer,
+	release_date date,
+	title varchar(255),
+	total integer,
+	artist_id bigint
+)
+;
+
+create table artist
+(
+	id bigint not null
+		constraint artist_pkey
+		primary key,
+	name varchar(255)
+)
+;
+
+alter table album
+	add constraint fkmwc4fyyxb6tfi0qba26gcf8s1
+foreign key (artist_id) references artist
+;
+
+create table song
+(
+	id bigint not null
+		constraint song_pkey
+		primary key,
+	features varchar(255),
+	is_confirmed boolean,
+	primary_artist varchar(255),
+	producers varchar(255),
+	title varchar(255),
+	track_no integer,
+	album_id bigint
+		constraint fkrcjmk41yqj3pl3iyii40niab0
+		references album
+)
+;
