662af313ebd6e7e770e3eb66051168111016a761
Matthew Williams <matthew.williams@impactradius.com>
2017-12-06 21:55:07 +0200

    Revert v1

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,38 +1,32 @@
-create sequence hibernate_sequence
+create table artist
+(
+	id bigint not null
+		constraint artist_pkey
+			primary key,
+	name varchar(255)
+)
 ;
 
 create table album
 (
 	id bigint not null
 		constraint album_pkey
-		primary key,
+			primary key,
 	confirmed integer,
 	release_date date,
 	title varchar(255),
 	total integer,
 	artist_id bigint
+		constraint fk_album_artist_id
+			references artist
 )
-;
-
-create table artist
-(
-	id bigint not null
-		constraint artist_pkey
-		primary key,
-	name varchar(255)
-)
-;
-
-alter table album
-	add constraint fkmwc4fyyxb6tfi0qba26gcf8s1
-foreign key (artist_id) references artist
 ;
 
 create table song
 (
 	id bigint not null
 		constraint song_pkey
-		primary key,
+			primary key,
 	features varchar(255),
 	is_confirmed boolean,
 	primary_artist varchar(255),
@@ -40,7 +34,8 @@ create table song
 	title varchar(255),
 	track_no integer,
 	album_id bigint
-		constraint fkrcjmk41yqj3pl3iyii40niab0
-		references album
+		constraint fk_song_album_id
+			references album
 )
 ;
+
